47036c144840364f5a024a16a351c5fd7ca0a5e9
Kristina <kristina.skoro@yandex.ru>
2023-05-21 13:58:33 +0300

    Добавила модели, сериалайзер, вьюсеты и urls для reviews и comments

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,23 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 
 from . import views
+from api.views import CommentViewSet, ReviewViewSet
+
+router_v1 = routers.DefaultRouter()
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments')
 
 urlpatterns = [
     path(
         'redoc/', views.redoc,
         name='redoc'
     ),
+    path('v1/', include(router_v1.urls)),
 ]
