4c979182a16c08e12d973f52d7d9b165d12ed353
Kristina <kristina.skoro@yandex.ru>
2023-05-26 09:04:49 +0300

    изменила verbose_name для поля slug

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -15,7 +15,7 @@ class GenreCategoryBaseClass(models.Model):
     )
     slug = models.SlugField(
         max_length=50,
-        verbose_name='ID',
+        verbose_name='Слаг',
         unique=True
     )
 
