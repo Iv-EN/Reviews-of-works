f81bfc380c178980523d8c609e1335cf2378d15e
AvailableNow <currently.out.of.service@gmail.com>
2023-05-27 18:50:12 -0400

    fix_urls

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -6,8 +6,13 @@ from .views import UserViewSet, create_user, create_token
 router_v1 = routers.DefaultRouter()
 router_v1.register('users', UserViewSet)
 
+api_v1_urls = [
+    path('auth/signup/', create_user, name='create_user'),
+    path('auth/token/', create_token, name='create_token'),
+]
+
+
 urlpatterns = [
-    path('v1/auth/signup/', create_user, name='create_user'),
-    path('v1/auth/token/', create_token, name='create_token'),
+    path('v1/', include(api_v1_urls)),
     path('v1/', include(router_v1.urls)),
 ]
