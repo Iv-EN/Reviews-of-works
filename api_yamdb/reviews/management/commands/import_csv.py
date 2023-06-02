6f342476d31e578a7c1fff6a7f20f9951834bbf8
Iv-EN <engenivanov@yandex.ru>
2023-06-02 10:14:03 +0300

    fixed import_csv

--- api_yamdb/reviews/management/commands/import_csv.py
+++ api_yamdb/reviews/management/commands/import_csv.py
@@ -1,5 +1,4 @@
 import csv
-import codecs
 import os
 
 from django.conf import settings
@@ -37,7 +36,7 @@ class Command(BaseCommand):
 
     def load_model(self, model_name, field_names):
         model, file_path = NAME_MODEL_FILE.get(model_name)
-        with open(self.get_csv_file(file_path)) as file:
+        with open(self.get_csv_file(file_path), encoding='utf-8') as file:
             reader = csv.reader(file, delimiter=',')
             self.clear_model(model)
             line = 0
@@ -62,8 +61,8 @@ class Command(BaseCommand):
         self.load_model('genre', ['id', 'name', 'slug'])
 
     def adding_genre_to_title(self):
-        with codecs.open(self.get_csv_file('genre_title.csv'), 'r',
-                         encoding='utf-8', errors='ignore') as file:
+        with open(self.get_csv_file('genre_title.csv'),
+                  encoding='utf-8') as file:
             reader = csv.reader(file, delimiter=',')
             line = 0
             for row in reader:
@@ -75,8 +74,7 @@ class Command(BaseCommand):
         self.print_to_terminal(f'{line - 1} objects added to genre_title')
 
     def load_title(self):
-        self.load_model('title', [
-            'id', 'name', 'year', 'category_id'])
+        self.load_model('title', ['id', 'name', 'year', 'category_id'])
         self.adding_genre_to_title()
 
     def load_reviews(self):
